e86a38fc3eb0f88fda3d085e845879acb1c110d7
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2024-04-04 23:44:45 +0200

    Add python formatter, linter and LSP to helix

--- pc/aconfmgr/40-utility.sh
+++ pc/aconfmgr/40-utility.sh
@@ -8,6 +8,10 @@ AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage helix # A post-modern modal text editor
 AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal emulator
 AddPackage libnotify # Library for sending desktop notifications
+AddPackage pyright # Type checker for the Python language
+AddPackage python-lsp-black # python-lsp-server plugin that adds support to black autoformatter, forked from pyls-black
+AddPackage python-lsp-server # Fork of the python-language-server project, maintained by the Spyder IDE team and the community
+AddPackage ruff-lsp # Language Server Protocol implementation for Ruff
 AddPackage rustup # The Rust toolchain installer
 AddPackage teamspeak3 # TeamSpeak is software for quality voice communication via the Internet
 AddPackage toml-bombadil # Adotfile manager with templating

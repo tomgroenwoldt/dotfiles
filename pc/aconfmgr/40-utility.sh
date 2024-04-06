2874ddc57935c149af48061124fb132efb0b74bf
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2024-04-06 13:27:53 +0200

    Replace eww with waybar and add rounded corners

--- pc/aconfmgr/40-utility.sh
+++ pc/aconfmgr/40-utility.sh
@@ -3,7 +3,6 @@ AddPackage brightnessctl # Lightweight brightness control tool
 AddPackage btop # A monitor of system resources, bpytop ported to C++
 AddPackage dolphin # KDE File Manager
 AddPackage dunst # Customizable and lightweight notification-daemon
-CopyFile /usr/bin/eww 755
 AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage helix # A post-modern modal text editor
 AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal emulator

7f3b462e5d7ac85a2aee76143a4a8701c62ed497
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2024-08-26 23:10:23 +0200

    Use Fira Code as font

--- pc/aconfmgr/50-appearance.sh
+++ pc/aconfmgr/50-appearance.sh
@@ -1,8 +1,8 @@
 AddPackage noto-fonts # Google Noto TTF fonts
 AddPackage noto-fonts-cjk # Google Noto CJK fonts
 AddPackage noto-fonts-emoji # Google Noto emoji fonts
+AddPackage ttf-fira-code # Monospaced font with programming ligatures
 AddPackage ttf-roboto # Google's signature family of fonts
 AddPackage ttf-roboto-mono-nerd # Patched font Roboto Mono from nerd fonts library
 
 AddPackage --foreign ttf-material-icons-git # Material design icons is the official icon set from Google.
-AddPackage --foreign ttf-comic-mono-git # like the famous font but monospace and with adjusted metric
